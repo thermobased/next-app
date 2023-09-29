36128a1d139314a1e81e10c51414146259fc2fdb
Andrey Oleinikov <oleinikov.2000.ao@gmail.com>
2023-09-29 07:53:12 +0600

    displaying one txt file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import * as fs from "fs";
 
-
-//page tsx
+const userFiles = fs.readFileSync('./src/pages/page1.txt', 'utf-8');
 
 
 export default function Home() {
   return (
+
+     //
+
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
@@ -14,12 +17,18 @@ export default function Home() {
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
         <div>
+
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
+
+
+
             By{' '}
+
+
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -42,6 +51,8 @@ export default function Home() {
           priority
         />
       </div>
+
+      <div>{userFiles}</div>
 
       <div className={styles.grid}>
         <a
